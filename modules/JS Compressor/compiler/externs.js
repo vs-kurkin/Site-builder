6a0b8447343e1ae3e348c2948d2c416764d6ca4b
Vlad <b-vladi@cs-console.ru>
2012-01-30 11:37:11 +0400

    Update copyright.

--- modules/JS Compressor/compiler/externs.js
+++ modules/JS Compressor/compiler/externs.js
@@ -1,7 +1,7 @@
 /*
- Copyright (c) Site-builder, 2012.
- Source: https://github.com/B-Vladi/Site-builder
- Vlad Kurkin, b-vladi@cs-console.ru.
+ * Copyright (c) Site-builder, 30.1.2012.
+ * Source: https://github.com/B-Vladi/Site-builder
+ * Author: Vlad Kurkin, b-vladi@cs-console.ru.
  */
 
 var jQuery, $;
